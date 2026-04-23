6b7220ca6ec93921cc7fad1ff57b2cfda15b4e52
Neidis De Oleo <deoleoneidis00@gmail.com>
2026-04-23 13:18:21 -0400

    Proyecto DevOps listo con Docker

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const app = require("../app");
+
+test("GET / devuelve Hola Mundo", async () => {
+  const res = await request(app).get("/");
+  expect(res.text).toBe("Hola Mundo DevOps");
+  expect(res.statusCode).toBe(200);
+});
